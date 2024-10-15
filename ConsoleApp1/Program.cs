f9a4eae77bf1d6f22ef589f57af833824603d90e
Администратор <@34-5>
2024-10-15 10:47:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите два числа, разделённых пробелом:");
+            string input = Console.ReadLine();
+
+            string[] inputs = input.Split(' ');
+
+            if (inputs.Length == 2 &&
+                double.TryParse(inputs[0], out double number1) &&
+                double.TryParse(inputs[1], out double number2))
+            {
+                Console.WriteLine($"Вы ввели числа: {number1} и {number2}");
+
+                if (number1 > number2)
+                {
+                    Console.WriteLine($"{number1} > {number2}");
+                }
+                else if (number1 < number2)
+                {
+                    Console.WriteLine($"{number1} < {number2}");
+                }
+                else
+                {
+                    Console.WriteLine($"{number1} = {number2}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, убедитесь, что вы ввели два числа, разделённых пробелом.");
+            }
+        }
+    }
+}
